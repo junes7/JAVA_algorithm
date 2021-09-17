458acbfb574989be2b42e46b284f816472d21cae
junes7 <2junes@naver.com>
2021-09-18 08:01:06 +0900

    review how to print string with java

---
+++ CodeUp/Main1001.java
@@ -0,0 +1,6 @@
+// 출력하기 01(print string)
+public class Main1001{
+    public static void main(String[] args){
+        System.out.println("Hello");
+    }
+}
---
+++ CodeUp/Main1002.java
@@ -0,0 +1,6 @@
+// 출력하기 02(print string)
+public class Main1002{
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+}
